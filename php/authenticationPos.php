63ad5eae9132137e4f2bc9c84eb35bad2c57a9bd
bohanoleksandr <bohanoleksandr@gmail.com>
2015-05-09 17:21:44 +0300

    Адміністративна частина повертається
    Поки що приведена до розуму сторінка авторизації адміністратора. А також інструкція для продавця відтепер сяє блиском.

--- php/authenticationPos.php
+++ php/authenticationPos.php
@@ -4,30 +4,50 @@ include_once('connectdb.php');
 $login = $_POST['login'];
 $pass = $_POST['password'];
 $shop_id = $_POST['selectedShop'];
-$shop_name = null;
-//$role = $_POST['role'];
 $page = $_SERVER['HTTP_REFERER'];
 
-$res1 = mysql_query("SELECT `name` FROM `stores` WHERE `id`='".$shop_id."';");
-$row1 = mysql_fetch_row($res1);
-$shop_name = $row1[0];
 
-$res2 = mysql_query("SELECT `id`, `password`, `name`, `role` FROM `workers` WHERE `login`='".$login."';");
-$row2 = mysql_fetch_row($res2);
-$needed_password = $row2[1];
-$worker = $row2[2];
+$res1 = mysql_query("SELECT `id`, `password`, `name`, `role` FROM `workers` WHERE `login`='".$login."';");
+$row1 = mysql_fetch_array($res1);
+$needed_password = $row1[1];
+$worker = $row1[2];
+$role = $row1[3];
 
-if ($res2){
-//    $info['shop_id'] = $shop_id;
-//    $info['shop_name'] = $shop_name;
-//    $info['worker'] = $worker;
-    if($needed_password === $pass  && $shop_name){
-        setcookie("workerId", $row2[0]);
-        setcookie("shopId", $shop_id);
-        header('Location: ../store/workspace.html');
+if ($row1 && $needed_password===$pass) {
+    if (strpos ($page, "admin")) {
+        $aaa = "admin";
+
+        if ($role == 1) {
+            setcookie("workerId", $row1[0]);
+            echo "В розробці";
+//            header ('Location: ../admin/admin.html');
+        } else {
+            echo "<script type='text/javascript'>alert('У вас немає прав адміністратора');</script>";
+            linkBack($page);
+        }
+
     } else {
-        header('Location: '.$page);
+        $aaa = "store";
+        $res2 = mysql_query("SELECT `name` FROM `stores` WHERE `id`='".$shop_id."';");
+        $row2 = mysql_fetch_array($res2);
+        if ($row2) {
+            setcookie("workerId", $row1[0]);
+            setcookie("shopId", $shop_id);
+            header('Location: ../store/workspace.html');
+        } else {
+            echo "<script type='text/javascript'>alert('Ви не обрали кав’ярню');</script>";
+            linkBack($page);
+        }
+
     }
+} else {
+    $aaa = "no login";
+//    header('Location: '.$page);
+    echo "<script type='text/javascript'>alert('Неправильний логін або пароль');</script>";
+    linkBack($page);
 }
 
+function linkBack ($page){
+    echo "<div style='text-align: center'><a href='".$page."'>Назад</a></div>";
+}
 ?>
