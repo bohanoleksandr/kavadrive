b7a591e4830ea0258f939d707ce122036b68a131
Oleksandr <bohanoleksandr@gmail.com>
2015-08-10 01:20:12 +0300

    Краса на сторінці з анкетами партнерів
    А окрім того іще і доповнений фунціонал датою заповнення та виведенням
    id, виводяться не всі замовлення, а лише "видимі" у базі даних

--- js/adminScripts.js
+++ js/adminScripts.js
@@ -1,6 +1,21 @@
+﻿var month_array = {
+    0: 'січня',
+    1: 'лютого',
+    2: 'березня',
+    3: 'квітня',
+    4: 'травня',
+    5: 'червня',
+    6: 'липня',
+    7: 'серпня',
+    8: 'вересня',
+    9: 'жовтня',
+    10: 'листопада',
+    11: 'грудня'
+};
+
 var forms = [];
 
-function Form (coords, name, mail, skype, cell_phone, contact_person, address, job_phone) {
+function Form(coords, name, mail, skype, cell_phone, contact_person, address, job_phone, filling_date) {
     this.coords = coords;
     this.name = name;
     this.mail = mail;
@@ -9,35 +24,40 @@ function Form (coords, name, mail, skype, cell_phone, contact_person, address, j
     this.contact_person = contact_person;
     this.address = address;
     this.job_phone = job_phone;
+    this.filling_date = filling_date.replace(/-/gi, '/');
 }
 
-function receiptData(){
-    $.ajax ({
+function receiptData() {
+    $.ajax({
         url: '../php/getPartnersForms.php',
         type: 'POST',
         cache: false,
-        success: function(msg){
-            var response=JSON.parse(msg);
+        success: function (msg) {
+            var response = JSON.parse(msg);
             var dummy = $("#dummy");
+            var filling_date;
             for (var i in response) {
                 var row = response[i];
-                forms[row[0]] = new Form (row[1], row[2], row[3], row[4], row[5], row[6], row[7], row[8]);
+                forms[row[0]] = new Form(row[1], row[2], row[3], row[4], row[5], row[6], row[7], row[8], row[9]);
+                filling_date = new Date(forms[row[0]].filling_date);
                 dummy.clone().attr({"id": 'form-' + row[0]})
-                    .appendTo("body")
-                    .children("caption").text(forms[row[0]].name).parent()
-                    .children("tbody").children("tr.coords").children("td").text(forms[row[0]].coords).parent().parent()
-                    .children("tr.mail").children("td").text(forms[row[0]].mail).parent().parent()
-                    .children("tr.job_phone").children("td").text(forms[row[0]].job_phone).parent().parent()
-                    .children("tr.cell_phone").children("td").text(forms[row[0]].cell_phone).parent().parent()
-                    .children("tr.skype").children("td").text(forms[row[0]].skype).parent().parent()
-                    .children("tr.address").children("td").text(forms[row[0]].address).parent().parent()
-                    .children("tr.contact_person").children("td").text(forms[row[0]].contact_person);
+                    .appendTo("body").children("div.block_header").children("span").text(row[0]).parent().parent()
+                    .children("table")
+                    .children("caption").children("div.name").text(forms[row[0]].name).parent()
+                    .children("div.fillingDate").text(filling_date.getDate() + " " + month_array[filling_date.getMonth()]).parent().parent()
+                    .children("tbody").children("tr.coords").children("td.essence").text(forms[row[0]].coords).parent().parent()
+                    .children("tr.mail").children("td.essence").text(forms[row[0]].mail).parent().parent()
+                    .children("tr.job_phone").children("td.essence").text(forms[row[0]].job_phone).parent().parent()
+                    .children("tr.cell_phone").children("td.essence").text(forms[row[0]].cell_phone).parent().parent()
+                    .children("tr.skype").children("td.essence").text(forms[row[0]].skype).parent().parent()
+                    .children("tr.address").children("td.essence").text(forms[row[0]].address).parent().parent()
+                    .children("tr.contact_person").children("td.essence").text(forms[row[0]].contact_person);
             }
             dummy.remove();
         }
     })
 }
 
-$(document).ready (function() {
+$(document).ready(function () {
     receiptData();
 });
--- php/getPartnersForms.php
+++ php/getPartnersForms.php
@@ -3,11 +3,11 @@ include_once('connectdb.php');
 
 $partnersForms = array();
 
-$result = mysql_query("SELECT * FROM partners");
+$result = mysql_query("SELECT * FROM partners WHERE visibility = 1;");
 
 while ($form = mysql_fetch_assoc($result)){
     $partnersForms[] = array($form['id'], $form['coords'], $form['name'], $form['mail'], $form['skype'],
-        $form['cell_phone'], $form['contact_person'], $form['address'], $form['job_phone']);
+        $form['cell_phone'], $form['contact_person'], $form['address'], $form['job_phone'], $form['filling_date']);
 }
 
 echo json_encode($partnersForms);
